b78b4a8157cf91349fdcd396fbe6567ab3ca8dad
fabioseixas <dev.fabioseixas@gmail.com>
2023-03-07 17:56:39 -0300

    feat: start offline web app
    - listContacts use cache
    - start service worker to cache source files

--- src/index.js
+++ src/index.js
@@ -5,6 +5,33 @@ import App from './components/App';
 const container = document.getElementById('root');
 const root = createRoot(container);
 
+const setupSw = () => {
+  if ("serviceWorker" in navigator) {
+    if (process.env.NODE_ENV == 'development') {
+      // on production, the SW is OneSignalSDKWorker.js
+      navigator.serviceWorker.register(new URL(`${window.location.origin}/serviceWorker.js`), {
+        scope: '/'
+      }).then((registration) => {
+        const sw = registration.waiting
+          || registration.installing
+          || registration.active
+        console.log('local sw registered: ', sw)
+      })
+    }
+
+    if (navigator.serviceWorker.controller) {
+      console.log('sw alredy registered: ', navigator.serviceWorker.controller.scriptURL)
+    }
+
+    navigator.serviceWorker.oncontrollerchange = (e) => {
+      console.log('controller changed')
+      console.log(e)
+    }
+  }
+}
+
+setupSw()
+
 root.render(
   <React.StrictMode>
     <App />
---
+++ src/services/utils/localDataSource.js
@@ -0,0 +1,48 @@
+import ContactMapper from '../mappers/contactMapper'
+
+class ContactsLocalDataSource {
+
+  listContacts(orderBy = 'asc') {
+    const contacts = localStorage.getItem('@MyContacts:contacts')
+    if (!contacts) return null
+    const parsedContacts = JSON.parse(contacts)
+    const arrayContacts = Object.values(parsedContacts)
+    if (!arrayContacts.length) return null
+    return arrayContacts.map(ContactMapper.toDomain).sort((a, b) => {
+      if (orderBy === 'asc') {
+        if (a.name.length < b.name.length) return 1
+        return -1
+      }
+      if (a.name.length > b.name.length) return 1
+      return -1
+    })
+  }
+
+  saveContacts(contacts) {
+    console.log('save contacts: ', contacts)
+    const contactsToPersist = contacts.map(ContactMapper.toPersistence)
+    localStorage.setItem('@MyContacts:contacts', JSON.stringify(contactsToPersist))
+    return true
+  }
+
+  // async getContactById(id) {
+  //   const contact = await this.httpClient.get(`/contacts/${id}`);
+  //   return ContactMapper.toDomain(contact);
+  // }
+
+  // createContact(contact) {
+  //   const body = ContactMapper.toPersistence(contact);
+  //   return this.httpClient.post('/contacts', { body });
+  // }
+
+  // updateContact(id, contact) {
+  //   const body = ContactMapper.toPersistence(contact);
+  //   return this.httpClient.put(`/contacts/${id}`, { body });
+  // }
+
+  // deleteContact(id) {
+  //   return this.httpClient.delete(`/contacts/${id}`);
+  // }
+}
+
+export default new ContactsLocalDataSource();
